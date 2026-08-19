f4979ac64670be68f3eb66dc5ad279c9cddc99c6
swadique@conversedatasolutions.com <swadique@conversedatasolutions.com>
2026-08-19 11:28:31 +0000

    BBBBBBB commit

--- test_bundle/test_bundle/src/notebooks/sample_etl.py
+++ test_bundle/test_bundle/src/notebooks/sample_etl.py
@@ -16,7 +16,7 @@
 from pyspark.sql import functions as F
 from datetime import datetime
 
-# Create sample data new comment added  AAAAAAAAAAAAAAAAAAAAAA
+# Create sample data new comment added  BBBBBBBBBBB
 data = [
     (1, "Product A", 100.50, "2024-01-01"),
     (2, "Product B", 250.75, "2024-01-02"),
